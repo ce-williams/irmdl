81a3820cdcebe9c52f68ab3a1a268151c9f6838b
cewilliams <cewilliams@csbsju.edu>
2019-04-02 02:06:37 -0500

    model

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# run using python not python3
 # tensorflow library import
 import tensorflow as tf
 
@@ -7,9 +8,25 @@ mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# normalize data
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.softmax))
+
+
+model.compile(optimer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3)
+
 
 # import matplot lib
 import matplotlib.pyplot as plt
 
-plt.imshow(x_train[0])
+plt.imshow(x_train[0], cmap = plt.cm.binary)
 plt.show()
+
+print(x_train[0])
